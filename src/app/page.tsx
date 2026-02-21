8ae86a65d72feacb3c8cfbaa47c065e4348f198c
Ranjith Reddy KV <ranjithreddykv27@gmail.com>
2026-02-21 18:20:41 +0530

    authentication setup successful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,62 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
+"use client";
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signUp, useSession,signOut } from "@/lib/auth-client";
+import { toast } from "sonner";
+
 export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const {data:session} = useSession();
+  const BASE_URL = "http://localhost:3000";
+  async function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+    try {
+      e.preventDefault();
+      const res = await signUp.email({ name, email, password });
+      //if (res.error) throw new Error("Error occoure while signup");
+      toast.success("User signup successful");
+      console.log(res.data);
+    } catch (error) {
+      toast.error(error instanceof Error ? error.message:"Error occoured while signing up")
+      console.log(error);
+    }
+  
+  }
+  if(session){
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={()=> signOut()}>Logout</Button>
+      </div>
+    )
+  }
+
   return (
-    <div className="text-4xl font-bold text-green-500">
-      Hello world
-      <Button variant={"outline"}>Hello</Button>
+    <div className="flex flex-col justify-center items-center max-w-xl">
+      <form action="" onSubmit={handleSubmit} className="flex flex-col items-center justify-center  w-full space-y-2 p-5  ">
+        <Input
+          
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="email"
+          type={"email"}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          type={"password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button variant={"outline"}>Login</Button>
+      </form>
     </div>
   );
 }
