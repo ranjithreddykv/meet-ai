3935048b719aff3fb07701be7427a57aa7ca497c
Ranjith Reddy KV <ranjithreddykv27@gmail.com>
2026-02-25 22:10:41 +0530

    agent form and getmany and gt one methods implemented

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,28 +1,39 @@
 import {
   AgentsViewError,
   AgentView,
-} from "@/modules/agents/server/ui/agents-view";
+} from "@/modules/agents/ui/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
-import { AgnetsViewLoading } from "../../../modules/agents/server/ui/agents-view";
+import { AgnetsViewLoading } from "../../../modules/agents/ui/agents-view";
 import { ErrorBoundary } from "react-error-boundary";
-import { ResponsiveDialog } from "@/components/responsive-dialog";
-import { Button } from "@/components/ui/button";
-const Page = () => {
+import AgentListHeader from "@/modules/agents/ui/components/agent-list-header";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+const Page = async() => {
+
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+  if(!session){
+    redirect("/sign-in")
+  }
+
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
-  
-  
-  
+
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<AgnetsViewLoading />}>
-        <ErrorBoundary fallback={<AgentsViewError />}>
-          <AgentView />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
+    <>
+      <AgentListHeader/>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<AgnetsViewLoading />}>
+          <ErrorBoundary fallback={<AgentsViewError />}>
+            <AgentView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
   );
 };
 
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,5 +1,8 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { cache } from "react";
+
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
@@ -20,3 +23,13 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "Unauthorized" });
+  }
+  return next({ ctx: { ...ctx, auth: session } });
+});
