f61910dfc4fc9f6ae3e06c14e167d566fb43969f
Ranjith Reddy KV <ranjithreddykv27@gmail.com>
2026-03-01 10:25:39 +0530

    Update and delete agent feature added

--- src/modules/agents/schemas.ts
+++ src/modules/agents/schemas.ts
@@ -4,3 +4,10 @@ export const agentInsertSchema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
   instructions: z.string().min(1, { message: "Instruction is required" }),
 });
+
+export const agentDeleteSchema = z.object({
+  id:z.string().min(1,{message:"Agent id is required"})
+})
+export const agentUpdateSchema =agentInsertSchema.extend( {
+  id: z.string().min(1, { message: "Agent id is required" }),
+});
