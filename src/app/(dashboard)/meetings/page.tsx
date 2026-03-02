0910d88fada805859bc82c82506d68099d03416e
Ranjith Reddy KV <ranjithreddykv27@gmail.com>
2026-03-02 19:37:38 +0530

    meeting update and new meeting form added

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,25 +1,39 @@
+import { auth } from "@/lib/auth";
+import MeetingsListHeader from "@/modules/meetings/ui/components/meeting-list-header";
 import {
   MeetingsView,
   MeetingsViewError,
   MeetingsViewLoading,
-} from "@/modules/meetings/server/ui/views/meetingView";
+} from "@/modules/meetings/ui/views/meetingView";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import React from "react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers:await headers()
+  })
+  if(!session){
+    redirect("/sign-in")
+  }
+
+
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<MeetingsViewLoading />}>
-        <ErrorBoundary fallback={<MeetingsViewError />}>
-          <MeetingsView />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
+    <>
+    <MeetingsListHeader/>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<MeetingsViewLoading />}>
+          <ErrorBoundary fallback={<MeetingsViewError />}>
+            <MeetingsView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
   );
 }
